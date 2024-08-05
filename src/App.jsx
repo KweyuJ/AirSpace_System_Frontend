eb6b3d34494e1a9fbd93533e9cd63792a0a4d12e
iannjau <lantanacreations@gmail.com>
2024-08-05 09:17:45 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
+import { FaHome, FaPlane, FaHotel, FaPhone, FaSignInAlt } from 'react-icons/fa';
 import Home from './components/Home';
 import Flights from './components/Flights';
 import Hotels from './components/Hotels';
 import ContactUs from './components/ContactUs';
 import Login from './components/Login';
 import logo from './assets/logo.png';
+import './index.css';
 
 function App() {
   return (
@@ -14,19 +16,19 @@ function App() {
         <img src={logo} alt="Logo" className="logo" />
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/"><FaHome /> Home</Link>
           </li>
           <li>
-            <Link to="/flights">Flights</Link>
+            <Link to="/flights"><FaPlane /> Flights</Link>
           </li>
           <li>
-            <Link to="/hotels">Hotels</Link>
+            <Link to="/hotels"><FaHotel /> Hotels</Link>
+          </li>
+          <li className="contactUs">
+            <Link to="/contactus"><FaPhone /> Contact us</Link>
           </li>
           <li>
-            <Link to="/contactus">Contact us</Link>
-          </li>
-          <li>
-            <Link to="/login">Log in</Link>
+            <Link to="/login"><FaSignInAlt /> Log in</Link>
           </li>
         </ul>
       </nav>
