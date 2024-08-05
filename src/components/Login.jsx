d1642ab37552e82e4b51b8afc6c87a1d4e9d9cc1
Joy <kweyuayieko@gmail.com>
2024-08-05 17:30:26 +0300

    Feat:Created login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,159 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Grid, Typography, TextField, Button, IconButton, Box, Snackbar, InputAdornment } from '@mui/material';
+import { ArrowBack as ArrowBackIcon, Visibility, VisibilityOff, Email } from '@mui/icons-material';
+import { Link, useNavigate } from 'react-router-dom';
+import image from "../assets/logo.png";
 
-function Login() {
-  return <h2>Login Page</h2>;
-}
+const SignInOptions = ({ handleOpen }) => {
+  return (
+    <div>
+      <Typography variant="h5" style={{ marginBottom: '20px' }}>Let's log you in</Typography>
+      <Grid container direction="column" spacing={2}>
+        <Grid item>
+          <div
+            onClick={() => handleOpen('email')}
+            style={{ display: "flex", alignItems: 'center', padding: "15px", width: '250px', boxShadow: 'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px', cursor: "pointer" }}
+          >
+            <Email fontSize="large" style={{ color: '#032541' }} />
+            <Typography style={{ color: '#032541', marginLeft: '10px' }}>Log In with email</Typography>
+          </div>
+        </Grid>
+        <Grid item>
+          <Link to='/signup' style={{ textDecoration: 'none' }}>
+            <Typography style={{ textAlign: 'center', marginTop: '10px', cursor: 'pointer', color: '#007BFF' }}>Don't have an account? Sign up</Typography>
+          </Link>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+const SignInForm = ({ signInWithEmail, handleClose }) => {
+  const [formData, setFormData] = useState({ email: '', password: '' });
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [successMessage, setSuccessMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const endpoint = 'http://127.0.0.1:5000/login/email';
+    const body = JSON.stringify({ email: formData.email, password: formData.password });
+
+    try {
+      const response = await fetch(endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: body,
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        localStorage.setItem('access_token', result.token);
+        localStorage.setItem('role', result.role);
+        localStorage.setItem('id', result.id);
+        setSuccessMessage('User signed in successfully!');
+        setTimeout(() => {
+          navigate('/', { replace: true });
+        }, 3000);
+      } else {
+        const errorResult = await response.json();
+        setError(errorResult.error || 'Login failed. Please check your credentials.');
+      }
+    } catch (error) {
+      setErrorMessage('Error: ' + error.message);
+    }
+
+    handleClose();
+  };
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
+  const handleSnackbarClose = () => {
+    setSuccessMessage('');
+    setErrorMessage('');
+  };
+
+  return (
+    <Box sx={{ padding: 4, width: '100%', bgcolor: 'background.paper', borderRadius: '8px' }}>
+      <IconButton onClick={handleClose} style={{ marginBottom: '1rem' }}>
+        <ArrowBackIcon />
+      </IconButton>
+      <h2>Log In</h2>
+      {error && <Typography color="error" style={{ marginBottom: '1rem' }}>{error}</Typography>}
+      {signInWithEmail && (
+        <form onSubmit={handleSubmit}>
+          <TextField label="Email" variant="outlined" fullWidth margin="normal" name="email" value={formData.email} onChange={handleInputChange} />
+          <TextField label="Password" type={passwordVisible ? "text" : "password"} variant="outlined" fullWidth margin="normal" name="password" value={formData.password} onChange={handleInputChange}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton onClick={togglePasswordVisibility}>
+                    {passwordVisible ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              )
+            }}
+          />
+          <Button type="submit" variant="contained" color="error" fullWidth style={{ marginTop: '1rem' }}>
+            Log In with Email
+          </Button>
+        </form>
+      )}
+      <Snackbar
+        open={!!successMessage || !!errorMessage}
+        autoHideDuration={6000}
+        onClose={handleSnackbarClose}
+        message={successMessage || errorMessage}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        ContentProps={{
+          style: {
+            backgroundColor: successMessage ? '#4CAF50' : '#D32F2F', // Green for success, Red for error
+            color: 'white'
+          },
+        }}
+      />
+    </Box>
+  );
+};
+
+const Login = () => {
+  const [signInWithEmail, setSignInWithEmail] = useState(false);
+
+  const handleOpen = (method) => {
+    setSignInWithEmail(method === 'email');
+  };
+
+  const handleClose = () => {
+    setSignInWithEmail(false);
+  };
+
+  return (
+    <Grid container justifyContent="center" alignItems="center" style={{ minHeight: '100vh', backgroundColor: 'white' }}>
+      <Grid item xs={12} sm={6}>
+        <img src={image} alt="Auth" style={{ width: '100%', height: 'auto', borderRadius: '8px' }} />
+      </Grid>
+      <Grid item xs={12} sm={6} container justifyContent="center" alignItems="center">
+        {signInWithEmail ? (
+          <SignInForm
+            signInWithEmail={signInWithEmail}
+            handleClose={handleClose}
+          />
+        ) : (
+          <SignInOptions handleOpen={handleOpen} />
+        )}
+      </Grid>
+    </Grid>
+  );
+};
 
 export default Login;
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,35 +1,60 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; 
-import { Grid, Typography, TextField, Button, IconButton, Box, InputAdornment, Snackbar } from '@mui/material';
-import { ArrowBack as ArrowBackIcon, Visibility, VisibilityOff } from '@mui/icons-material';
-
-import image from '../assets/logo.png';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  Grid,
+  Typography,
+  TextField,
+  Button,
+  IconButton,
+  Box,
+  InputAdornment,
+  Snackbar,
+} from "@mui/material";
+import {
+  ArrowBack as ArrowBackIcon,
+  Visibility,
+  VisibilityOff,
+} from "@mui/icons-material";
+import image from "../assets/logo.png";
 
 const styles = {
-  signUpOption: { display: "flex", alignItems: 'center', padding: "15px", width: '250px', boxShadow: 'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px', cursor: "pointer" },
-  signUpImage: { width: '100%', height: 'auto', borderRadius: '8px' },
-  container: { minHeight: '100vh', backgroundColor: 'white' },
-  backButton: { marginBottom: '1rem' },
-  formContainer: { padding: 4, width: '100%', bgcolor: 'background.paper', borderRadius: '8px' },
-  signUpWrapper: { marginTop: '20px', textAlign: 'center' },
+  signUpOption: {
+    display: "flex",
+    alignItems: "center",
+    padding: "15px",
+    width: "250px",
+    boxShadow:
+      "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px",
+    cursor: "pointer",
+  },
+  signUpImage: { width: "100%", height: "auto", borderRadius: "8px" },
+  container: { minHeight: "100vh", backgroundColor: "white" },
+  backButton: { marginBottom: "1rem" },
+  formContainer: {
+    padding: 4,
+    width: "100%",
+    bgcolor: "background.paper",
+    borderRadius: "8px",
+  },
+  signUpWrapper: { marginTop: "20px", textAlign: "center" },
 };
 
 const SignUp = () => {
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
   const [showSignInForm, setShowSignInForm] = useState(false);
   const [formData, setFormData] = useState({
-    title: '',
-    first_name: '',
-    last_name: '',
-    email: '',
-    phone_number: '',
-    password: '',
-    repeatPassword: '',
+    title: "",
+    first_name: "",
+    last_name: "",
+    email: "",
+    phone_number: "",
+    password: "",
+    repeatPassword: "",
     showPassword: false,
     showRepeatPassword: false,
   });
-  const [successMessage, setSuccessMessage] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
   const [formErrors, setFormErrors] = useState({
     title: false,
     first_name: false,
@@ -47,135 +72,202 @@ const SignUp = () => {
     }
   };
 
-  const handleShowPassword = () => {
-    setFormData({ ...formData, showPassword: !formData.showPassword });
-  };
-
-  const handleShowRepeatPassword = () => {
-    setFormData({ ...formData, showRepeatPassword: !formData.showRepeatPassword });
-  };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
-    if (formData.password !== formData.repeatPassword) {
-      setErrorMessage("Passwords do not match!");
-      setFormErrors({ ...formErrors, password: true, repeatPassword: true });
-      return;
-    }
-  
-    const requiredFields = ['first_name', 'last_name', 'email', 'phone_number', 'password', 'repeatPassword'];
-    let hasError = false;
-    const newFormErrors = { ...formErrors };
-  
-    requiredFields.forEach(field => {
-      if (!formData[field]) {
-        newFormErrors[field] = true;
-        hasError = true;
-      } else {
-        newFormErrors[field] = false;
-      }
-    });
-  
-    setFormErrors(newFormErrors);
-  
-    if (hasError) {
-      setErrorMessage('Please fill out all required fields.');
-      return;
-    }
-  
-    const user = {
+    const newErrors = {};
+    if (!formData.title) newErrors.title = true;
+    if (!formData.first_name) newErrors.first_name = true;
+    if (!formData.last_name) newErrors.last_name = true;
+    if (!formData.email) newErrors.email = true;
+    if (!formData.phone_number) newErrors.phone_number = true;
+    if (!formData.password) newErrors.password = true;
+    if (formData.password !== formData.repeatPassword)
+      newErrors.repeatPassword = true;
+
+    setFormErrors(newErrors);
+
+    if (Object.keys(newErrors).length) return;
+
+    const endpoint = "http://127.0.0.1:5000/signup";
+    const body = JSON.stringify({
       title: formData.title,
       first_name: formData.first_name,
       last_name: formData.last_name,
       email: formData.email,
       phone_number: formData.phone_number,
-      password: formData.password
-    };
-  
+      password: formData.password,
+    });
+
     try {
-      const response = await fetch('http://127.0.0.1:5000/users', {
-        method: 'POST',
+      const response = await fetch(endpoint, {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(user),
+        body: body,
       });
-    
+
       if (response.ok) {
         const result = await response.json();
-        localStorage.setItem('access_token', result.access_token);
-        setSuccessMessage('User signed up successfully!');
-        handleClose(); 
-        navigate('/login', { replace: true });
-      } else if (response.status === 409) {
-        setErrorMessage('Email already exists. Please use a different email.');
+        setSuccessMessage("User signed up successfully!");
+        setTimeout(() => {
+          navigate("/", { replace: true });
+        }, 3000);
       } else {
-        throw new Error(`Sign up failed: ${response.statusText}`);
+        const errorResult = await response.json();
+        setErrorMessage(errorResult.error || "Signup failed.");
       }
     } catch (error) {
-      setErrorMessage(`Error: ${error.message}`);
-    }    
+      setErrorMessage("Error: " + error.message);
+    }
+  };
+
+  const togglePasswordVisibility = () => {
+    setFormData({
+      ...formData,
+      showPassword: !formData.showPassword,
+    });
+  };
+
+  const toggleRepeatPasswordVisibility = () => {
+    setFormData({
+      ...formData,
+      showRepeatPassword: !formData.showRepeatPassword,
+    });
   };
 
   const handleSnackbarClose = () => {
-    setSuccessMessage('');
-    setErrorMessage('');
-  };
-
-  const handleOpen = () => {
-    setShowSignInForm(true);
-  };
-
-  const handleClose = () => {
-    setShowSignInForm(false);
+    setSuccessMessage("");
+    setErrorMessage("");
   };
 
   return (
-    <Grid container justifyContent="center" alignItems="center" sx={styles.container}>
+    <Grid container justifyContent="center" alignItems="center" style={styles.container}>
       <Grid item xs={12} sm={6}>
         <img src={image} alt="Auth" style={styles.signUpImage} />
       </Grid>
       <Grid item xs={12} sm={6} container justifyContent="center" alignItems="center">
-        <div style={styles.signUpWrapper}>
-          <Typography variant="h5" style={{ marginBottom: '20px' }}>Let's sign you up</Typography>
-          {showSignInForm ? (
-            <Box sx={styles.formContainer}>
-              <IconButton onClick={handleClose} style={styles.backButton}>
-                <ArrowBackIcon />
-              </IconButton>
-              <Typography variant="h4">Sign Up</Typography>
-              <form onSubmit={handleSubmit}>
-                <TextField label="Title" variant="outlined" fullWidth margin="normal" name="title" value={formData.title} onChange={handleInputChange} error={formErrors.title} />
-                <TextField label="First Name" variant="outlined" fullWidth margin="normal" name="first_name" value={formData.first_name} onChange={handleInputChange} required error={formErrors.first_name}/>
-                <TextField label="Last Name" variant="outlined" fullWidth margin="normal" name="last_name" value={formData.last_name} onChange={handleInputChange} required error={formErrors.last_name}/>
-                <TextField label="Email" variant="outlined" fullWidth margin="normal" name="email" value={formData.email} onChange={handleInputChange} required error={formErrors.email}/>
-                <TextField label="Phone Number" variant="outlined" fullWidth margin="normal" name="phone_number" value={formData.phone_number} onChange={handleInputChange} required error={formErrors.phone_number}/>
-                <TextField label="Password" variant="outlined" fullWidth margin="normal" type={formData.showPassword ? 'text' : 'password'} name="password" value={formData.password} onChange={handleInputChange} required error={formErrors.password} InputProps={{ endAdornment: (
+        <Box sx={styles.formContainer}>
+          <IconButton onClick={() => setShowSignInForm(!showSignInForm)} style={styles.backButton}>
+            <ArrowBackIcon />
+          </IconButton>
+          <Typography variant="h5">Sign Up</Typography>
+          <form onSubmit={handleSubmit}>
+            <TextField
+              label="Title"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+              name="title"
+              value={formData.title}
+              onChange={handleInputChange}
+              error={formErrors.title}
+              helperText={formErrors.title ? "Title is required" : ""}
+            />
+            <TextField
+              label="First Name"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+              name="first_name"
+              value={formData.first_name}
+              onChange={handleInputChange}
+              error={formErrors.first_name}
+              helperText={formErrors.first_name ? "First name is required" : ""}
+            />
+            <TextField
+              label="Last Name"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+              name="last_name"
+              value={formData.last_name}
+              onChange={handleInputChange}
+              error={formErrors.last_name}
+              helperText={formErrors.last_name ? "Last name is required" : ""}
+            />
+            <TextField
+              label="Email"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              error={formErrors.email}
+              helperText={formErrors.email ? "Email is required" : ""}
+            />
+            <TextField
+              label="Phone Number"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+              name="phone_number"
+              value={formData.phone_number}
+              onChange={handleInputChange}
+              error={formErrors.phone_number}
+              helperText={formErrors.phone_number ? "Phone number is required" : ""}
+            />
+            <TextField
+              label="Password"
+              type={formData.showPassword ? "text" : "password"}
+              variant="outlined"
+              fullWidth
+              margin="normal"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+              error={formErrors.password}
+              helperText={formErrors.password ? "Password is required" : ""}
+              InputProps={{
+                endAdornment: (
                   <InputAdornment position="end">
-                    <IconButton onClick={handleShowPassword} edge="end">
-                      {formData.showPassword ? <Visibility /> : <VisibilityOff />}
+                    <IconButton onClick={togglePasswordVisibility}>
+                      {formData.showPassword ? <VisibilityOff /> : <Visibility />}
                     </IconButton>
                   </InputAdornment>
-                ), }}/>
-                <TextField label="Repeat Password" variant="outlined" fullWidth margin="normal" type={formData.showRepeatPassword ? 'text' : 'password'} name="repeatPassword" value={formData.repeatPassword} onChange={handleInputChange} required error={formErrors.repeatPassword} InputProps={{ endAdornment: (
+                ),
+              }}
+            />
+            <TextField
+              label="Repeat Password"
+              type={formData.showRepeatPassword ? "text" : "password"}
+              variant="outlined"
+              fullWidth
+              margin="normal"
+              name="repeatPassword"
+              value={formData.repeatPassword}
+              onChange={handleInputChange}
+              error={formErrors.repeatPassword}
+              helperText={formErrors.repeatPassword ? "Passwords do not match" : ""}
+              InputProps={{
+                endAdornment: (
                   <InputAdornment position="end">
-                    <IconButton onClick={handleShowRepeatPassword} edge="end">
-                      {formData.showRepeatPassword ? <Visibility /> : <VisibilityOff />}
+                    <IconButton onClick={toggleRepeatPasswordVisibility}>
+                      {formData.showRepeatPassword ? <VisibilityOff /> : <Visibility />}
                     </IconButton>
                   </InputAdornment>
-                ), }}/>
-                <Button type="submit" variant="contained" color="primary" fullWidth>Sign Up</Button>
-              </form>
-              <Snackbar open={!!successMessage} autoHideDuration={6000} onClose={handleSnackbarClose} message={successMessage} />
-              <Snackbar open={!!errorMessage} autoHideDuration={6000} onClose={handleSnackbarClose} message={errorMessage} />
-            </Box>
-          ) : (
-            <div onClick={handleOpen} style={styles.signUpOption}>
-              <Typography variant="body1" style={{ flex: 1, textAlign: 'left' }}>Sign up with your email</Typography>
-            </div>
-          )}
-        </div>
+                ),
+              }}
+            />
+            <Button type="submit" variant="contained" color="error" fullWidth style={{ marginTop: "1rem" }}>
+              Sign Up
+            </Button>
+          </form>
+          <Snackbar
+            open={!!successMessage || !!errorMessage}
+            autoHideDuration={6000}
+            onClose={handleSnackbarClose}
+            message={successMessage || errorMessage}
+            anchorOrigin={{ vertical: "top", horizontal: "center" }}
+            ContentProps={{
+              style: {
+                backgroundColor: successMessage ? "#4CAF50" : "#D32F2F", // Green for success, Red for error
+                color: "white",
+              },
+            }}
+          />
+        </Box>
       </Grid>
     </Grid>
   );
