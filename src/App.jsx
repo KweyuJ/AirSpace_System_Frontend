81211535f9c6977a345d8980ad2e962e2947cc33
Joy <kweyuayieko@gmail.com>
2024-08-06 17:00:58 +0300

    Feat:removed extra css files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
-import { FaHome, FaPlane, FaHotel, FaPhone, FaSignInAlt, FaUserPlus } from 'react-icons/fa';
+import { FaHome, FaPlane, FaHotel, FaPhone, FaSignInAlt, FaUserPlus, FaInfoCircle } from 'react-icons/fa';
 import Home from './components/Home';
 import Flights from './components/Flights';
 import Hotels from './components/Hotels';
 import ContactUs from './components/ContactUs';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
+import AboutUs from './components/About';
 import logo from './assets/logo.png';
 import './index.css';
 
@@ -18,6 +19,9 @@ function App() {
         <ul>
           <li>
             <Link to="/"><FaHome /> Home</Link>
+          </li>
+          <li>
+            <Link to="/aboutus"><FaInfoCircle /> AboutUs</Link> 
           </li>
           <li>
             <Link to="/flights"><FaPlane /> Flights</Link>
@@ -39,11 +43,13 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/aboutus" element={<AboutUs />} />
           <Route path="/flights" element={<Flights />} />
           <Route path="/hotels" element={<Hotels />} />
           <Route path="/contactus" element={<ContactUs />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path='/aboutus' element={<SignUp />} />
         </Routes>
       </main>
     </div>
